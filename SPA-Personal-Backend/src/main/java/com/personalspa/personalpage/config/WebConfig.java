d6075b46dd706db2b3a19eb97de6da29e0d66552
WizardSustine <bustamante.juan1990@gmail.com>
2026-02-11 09:43:46 -0300

    Patching 0.9 enviroments problem

--- SPA-Personal-Backend/src/main/java/com/personalspa/personalpage/config/WebConfig.java
+++ SPA-Personal-Backend/src/main/java/com/personalspa/personalpage/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.personalspa.personalpage.enviroments.Enviroments;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
--- SPA-Personal-Backend/src/main/java/com/personalspa/personalpage/entities/JWTUtil.java
+++ SPA-Personal-Backend/src/main/java/com/personalspa/personalpage/entities/JWTUtil.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.personalspa.personalpage.enviroments.Enviroments;
-
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
